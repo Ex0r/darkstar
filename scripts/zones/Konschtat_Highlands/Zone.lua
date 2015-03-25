784f963bb54dd1c3a9adf289437b2a247582e430
rokDSP <sschwender@gmail.com>
2015-03-25 05:46:09 +0000

    Dig items for 13 zones
    dig diglet dig!

--- scripts/zones/Konschtat_Highlands/Zone.lua
+++ scripts/zones/Konschtat_Highlands/Zone.lua
@@ -11,6 +11,62 @@ require("scripts/globals/zone");
 require("scripts/globals/icanheararainbow");
 
 -----------------------------------
+-- Chocobo Digging vars
+-----------------------------------
+local itemMap = {
+                    -- itemid, abundance, requirement
+                    { 847, 13, DIGREQ_NONE },
+                    { 880, 165, DIGREQ_NONE },
+                    { 690, 68, DIGREQ_NONE },
+                    { 864, 80, DIGREQ_NONE },
+                    { 768, 90, DIGREQ_NONE },
+                    { 869, 63, DIGREQ_NONE },
+                    { 749, 14, DIGREQ_NONE },
+                    { 17296, 214, DIGREQ_NONE },
+                    { 844, 14, DIGREQ_NONE },
+                    { 868, 45, DIGREQ_NONE },
+                    { 642, 71, DIGREQ_NONE },
+                    { 4099, 147, DIGREQ_BURROW },
+                    { 4100, 147, DIGREQ_BURROW },
+                    { 845, 28, DIGREQ_BORE },
+                    { 842, 27, DIGREQ_BORE },
+                    { 843, 23, DIGREQ_BORE },
+                    { 1845, 22, DIGREQ_BORE },
+                    { 838, 19, DIGREQ_BORE },
+					{ 4570, 10, DIGREQ_MODIFIER },
+                    { 4487, 11, DIGREQ_MODIFIER },
+                    { 4409, 12, DIGREQ_MODIFIER },
+                    { 1188, 10, DIGREQ_MODIFIER },
+                    { 4532, 12, DIGREQ_MODIFIER },
+                };
+
+local messageArray = { DIG_THROW_AWAY, FIND_NOTHING, ITEM_OBTAINED };
+
+-----------------------------------
+-- onChocoboDig
+-----------------------------------
+function onChocoboDig(player, precheck)
+
+    -- Let's get the weather of the zone
+
+    local weather = player:getWeather();
+
+    if (weather ~= nil) then
+      if (weather >= 0 and weather <= 4) then
+        zoneWeather = "WEATHER_NONE";
+      elseif (weather > 4 and weather % 2 ~= 0) then -- If the weather is 5, 7, 9, 11, 13, 15, 17 or 19, checking for odd values
+        zoneWeather = "WEATHER_DOUBLE";
+      else
+        zoneWeather = "WEATHER_SINGLE";
+      end
+    else
+      zoneWeather = "WEATHER_NONE";
+    end
+    
+    return chocoboDig(player, itemMap, precheck, messageArray, zoneWeather);
+end;
+
+-----------------------------------
 -- onInitialize
 -----------------------------------
 
